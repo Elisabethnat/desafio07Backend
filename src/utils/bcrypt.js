888e93439047286f1f1be774b168d63102829678
Elisabeth <lelie_angel@hotmail.com>
2024-03-05 17:29:53 -0300

    cambios

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,14 +1,13 @@
-import 'dotenv/config'; //lo importo para el ejemplo
+import 'dotenv/config'; 
 import bcrypt from 'bcrypt';
 
-//encriptar la contraseña
+
 export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(parseInt(process.env.SALT)));
 
 const hashPasword = createHash('Coder');
-// console.log(hashPasword )
 
-//Validar contraseña encriptada
+
+
 export const validatePassword = (passwordSend, passwordBDD) => bcrypt.compareSync(passwordSend, passwordBDD);
 
-// console.log(validatePassword('Coder', hashPasword ))
 
